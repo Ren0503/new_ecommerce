58fe3a4414dd39c137b9bdc0690f6a98b2d40467
Ren0503 <illuminati0440@gmail.com>
2022-02-28 14:06:42 +0700

    Add middleware

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,50 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models';
+import { Request, Response, NextFunction } from '../types';
+import asyncHandler from 'express-async-handler';
+
+interface Decoded {
+    id?: string;
+    iat: number;
+    exp: number;
+}
+
+export const protect = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        let token;
+        const auth = req.headers.authorization;
+        if (auth && auth.startsWith('Bearer')) {
+            try {
+                token = auth.split(' ')[1];
+                const decoded = jwt.verify(token, process.env.JWT_SECRET!) as Decoded;
+                const user = await User.findById(decoded.id).select('-password');
+                if (!user) throw new Error('User Not found');
+                req.user = {
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    isAdmin: user.isAdmin
+                };
+                next();
+            } catch (error) {
+                console.error(error);
+                res.status(401);
+                throw new Error('Not authorized, token failed');
+            }
+        }
+        if (!token) {
+            res.status(401);
+            throw new Error('Not authorized, no token');
+        }
+    }
+);
+
+export const isAdmin = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        if (req.user && req.user.isAdmin) next();
+        else {
+            res.status(401);
+            throw new Error('Not authorized as an admin');
+        }
+    }
+);
